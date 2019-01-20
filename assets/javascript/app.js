b2b7e6ecaf65ef86b0909f0d9e140ec9d5280194
Amanda Carl <amandacarl@mba13-8-256-v610a.attlocal.net>
2019-01-19 21:58:19 -0600

    final additions on Satuday Jan19

---
+++ assets/javascript/app.js
@@ -0,0 +1,72 @@
+$(document).ready(function() {
+    var startTime = 30;
+    var questions = [
+        "question1",
+        "question2",
+        "question3",
+        "question4",
+        "question5",
+        "question6",
+        "question7",
+        "question8"
+    ];
+    var guessesCorrect = 0;
+    // correct answers
+    var guessesIncorrect = 0;
+    // questions guessed wrong
+    var noGuess = 0;
+    //questions not answered
+
+    function endGame() {
+        $("#startPage").css("display", "none");
+        $("#quizPage").css("display", "none");
+        $("#resultsPage").css("display", "block");
+        scoreAnswers();
+        $("#guessesCorrect").html(guessesCorrect);
+        $("#guessesIncorrect").html(guessesIncorrect);
+        $("#noGuess").html(noGuess);
+    }
+
+    function updateTimer() {
+        $("#timer").html(startTime--);
+        if (startTime == 0) {
+            endGame();
+        }
+    }
+
+    function startGame() {
+        $("#startPage").css("display", "none");
+        $("#quizPage").css("display", "block");
+        $("#resultsPage").css("display", "none");
+        updateTimer();
+        setInterval(updateTimer, 1000);
+    }
+
+    function scoreAnswers() {
+        for (var i=0; i < questions.length; i++) {
+            var questionName = questions[i];
+            if ($("input[name='"+questionName+"']:checked").val() === "true") {
+                guessesCorrect++;
+            } else if ($("input[name='"+questionName+"']:checked").val() === "false") {
+                guessesIncorrect++;
+            } else {
+                noGuess++;
+            }
+        }
+    }
+
+    $("#startButton").on("click", startGame);
+    $("#submitQuiz").on("click", endGame);
+
+
+    // Find the selected answer in the HTML.
+    // Handle what happens if the answer is correct.
+    // Handle what happens if the answer is wrong.
+
+    // need a way to collect answers - right and wrong
+    // need a way to select an answer option - look at help page
+
+
+
+
+});
